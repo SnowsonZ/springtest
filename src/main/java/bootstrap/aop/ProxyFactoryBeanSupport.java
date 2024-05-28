905901c0f68ee716d5a414a33f95f52d798d4ad2
Snowson <snowsonz@outlook.com>
2024-05-28 14:46:08 +0800

    update: minor update

--- src/main/java/bootstrap/aop/ProxyFactoryBeanSupport.java
+++ src/main/java/bootstrap/aop/ProxyFactoryBeanSupport.java
@@ -2,6 +2,7 @@ package bootstrap.aop;
 
 import org.springframework.aop.framework.ProxyFactoryBean;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
 
 /**
@@ -10,12 +11,7 @@ import org.springframework.stereotype.Component;
 @Component("water")
 public class ProxyFactoryBeanSupport extends ProxyFactoryBean {
 
-
-    Coffee coffee;
-
-    @Autowired
     public ProxyFactoryBeanSupport(Coffee coffee) throws ClassNotFoundException {
-        this.coffee = coffee;
         this.setInterceptorNames("loggerInterceptor", "loggerInterceptor");
         this.setProxyInterfaces(Coffee.class.getInterfaces());
         this.setTarget(coffee);
